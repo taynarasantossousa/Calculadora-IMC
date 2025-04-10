6c8ed3598b192a4bf141d6ce80f5879d9dfb84c6
Taynara <taynarasantossousa09@gmail.com>
2025-04-10 20:00:41 -0300

    projeto-calculadora-imc

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-var btnCalcular = document.querySelector("#btnCalcular")
-var peso = document.querySelector("#peso")
-var altura = document.querySelector("#altura")
-var res = document.querySelector(".res")
+var btnCalcular = document.querySelector("#btnCalcular");
+var peso = document.querySelector("#peso");
+var altura = document.querySelector("#altura");
+var res = document.querySelector(".res");
 
 function calcular(event) {
-    event.preventDefault()
-    var pesoValue = peso.value
-    var alturaValue = altura.value
+  event.preventDefault();
+  var pesoValue = peso.value;
+  var alturaValue = altura.value;
 
-    if(pesoValue == 0 || alturaValue == 0) {
-        btnCalcular.value = "ERRO"
-        return
-    }
+  if (pesoValue == 0 || alturaValue == 0) {
+    btnCalcular.value = "ERRO";
+    return;
+  }
 
-    var IMC = (pesoValue / Math.pow(alturaValue, 2)).toFixed(2)
+  var IMC = (pesoValue / Math.pow(alturaValue, 2)).toFixed(2);
 
-    if (IMC < 17) {
-        res.innerHTML = ` <div class= "status"> 
+  if (IMC < 17) {
+    res.innerHTML = ` <div class= "status"> 
         <p>Peso</p>
             <p>Altura</p>
             <p>IMC</p>
@@ -26,10 +26,11 @@ function calcular(event) {
             <p>${pesoValue}</p>
             <p>${alturaValue}</p>
             <p>${IMC}</p>
-            <p>Peso normal</p>
+            <p>Muito abaixo do peso</p>
          </div> `;
-    }else if (IMC >= 17 && IMC <= 18.49) {
-        ` <div class= "status"> 
+  } else if (IMC >= 17 && IMC <= 18.49) {
+    
+    res.innerHTML = ` <div class= "status"> 
         <p>Peso</p>
             <p>Altura</p>
             <p>IMC</p>
@@ -41,8 +42,9 @@ function calcular(event) {
             <p>${IMC}</p>
             <p>Abaixo do peso</p>
          </div> `;
-    }else if (IMC >= 18.5 && IMC <= 24.99) {
-        ` <div class= "status"> 
+  } else if (IMC >= 18.5 && IMC <= 24.99) {
+    
+    res.innerHTML = ` <div class= "status"> 
         <p>Peso</p>
             <p>Altura</p>
             <p>IMC</p>
@@ -54,8 +56,9 @@ function calcular(event) {
             <p>${IMC}</p>
             <p>Peso normal</p>
          </div> `;
-    }else if (IMC >= 25 && IMC <= 29.99) {
-        ` <div class= "status"> 
+  } else if (IMC >= 25 && IMC <= 29.99) {
+   
+    res.innerHTML = ` <div class= "status"> 
         <p>Peso</p>
             <p>Altura</p>
             <p>IMC</p>
@@ -67,8 +70,8 @@ function calcular(event) {
             <p>${IMC}</p>
             <p>Acima do peso</p>
          </div> `;
-    }else if (IMC >= 30 && IMC <= 34.99) {
-        ` <div class= "status"> 
+  } else if (IMC >= 30 && IMC <= 34.99) {
+    res.innerHTML = ` <div class= "status"> 
         <p>Peso</p>
             <p>Altura</p>
             <p>IMC</p>
@@ -80,8 +83,8 @@ function calcular(event) {
             <p>${IMC}</p>
             <p>Obesidade I</p>
          </div> `;
-    }else if (IMC >= 35 && IMC <= 39.99) {
-        ` <div class= "status"> 
+  } else if (IMC >= 35 && IMC <= 39.99) {
+    res.innerHTML = ` <div class= "status"> 
         <p>Peso</p>
             <p>Altura</p>
             <p>IMC</p>
@@ -93,8 +96,8 @@ function calcular(event) {
             <p>${IMC}</p>
             <p>Obesidade II (Severa)</p>
          </div> `;
-    }else {
-        ` <div class= "status"> 
+  } else {
+    res.innerHTML = ` <div class= "status"> 
         <p>Peso</p>
             <p>Altura</p>
             <p>IMC</p>
@@ -106,13 +109,11 @@ function calcular(event) {
             <p>${IMC}</p>
             <p>Obesidade III (Mórbida)</p>
          </div> `;
-    }
+  }
 
-    peso.value = ""
-    altura.value = ""
-    btnCalcular.value = "Refazer"
-
+  peso.value = "";
+  altura.value = "";
+  btnCalcular.value = "Refazer";
 }
 
-
-btnCalcular.addEventListener("click",calcular);
+btnCalcular.addEventListener("click", calcular);
